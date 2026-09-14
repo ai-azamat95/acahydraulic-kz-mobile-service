22d317dd2a2e8cd216b9df4b2444cf12af72908a
ai-azamat95 <manus@acahydraulic.kz>
2026-09-14 18:55:31 +0500

    Load new responsive catalog styling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,6 +8,7 @@ import superjson from "superjson";
 import App from "./App";
 import { getLoginUrl } from "./const";
 import "./index.css";
+import "./catalog-mobile.css";
 import "./i18n";
 
 const queryClient = new QueryClient();
